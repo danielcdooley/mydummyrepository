7eb3cd28d037d4bcbd60d5cf3a93d34b47cc4dd0
danielcdooley <48460680+danielcdooley@users.noreply.github.com>
2021-04-12 20:22:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 # The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 # Return the shuffled string.
 
-# Example:
 # Input: s = "art", indices = [1,0,2]
 # Output: "rat"
 
