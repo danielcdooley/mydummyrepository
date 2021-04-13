4da1f909a69ef1dd2a511949cdf3c83bc16c5a6a
Dan Dooley <daniel.c.dooley@gmail.com>
2021-04-13 03:15:38 +0000

    my initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+# Problem from LeetCode: https://leetcode.com/problems/shuffle-string/  
+
+# Given a string s and an integer array indices of the same length.
+# The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+# Return the shuffled string.
+
+# Example:
+# Input: s = "art", indices = [1,0,2]
+# Output: "rat"
+
+def shuffle_string(s, array_indices):
+  n = len(s)
+  t = [None] * n
+
+  for i, ch in enumerate(s):
+    output_index = array_indices[i]
+    t[output_index] = ch
+  return ''.join(t)
+
+shuffled_string = shuffle_string('art', [1, 0, 2])
+print(shuffled_string)
+
+
+
